2f72b7434f769b5a233b7efee2a9395b520c58e2
Mariam Elbaz <marouma.we.bas@gmail.com>
2025-07-12 19:35:39 +0300

    add validation to admin/place routes

--- routes/adminPlaceRoutes.js
+++ routes/adminPlaceRoutes.js
@@ -7,14 +7,16 @@ import {
 } from '../controllers/adminPlaceController.js';
 import { isAdmin } from '../middlewares/isAdmin.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { createPlaceValidation, updatePlaceValidation } from '../validations/validatePlace.js';
+import { validateInput } from '../middlewares/validateInput.js';
 
 const router = express.Router();
 
 // Route for creating a new place
-router.post('/place',authMiddleware, isAdmin, createPlace);
+router.post('/place',authMiddleware, isAdmin, createPlaceValidation, validateInput, createPlace);
 
 // Route for updating a place by ID
-router.put('/place/:id',authMiddleware, isAdmin, updatePlace);
+router.put('/place/:id',authMiddleware, isAdmin, updatePlaceValidation, validateInput, updatePlace);
 
 // Route for toggling visibility (soft delete) of a place
 router.put('/place/:id/visibility',authMiddleware, isAdmin, toggleVisibility);
