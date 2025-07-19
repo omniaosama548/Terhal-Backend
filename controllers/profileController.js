3bea14623334a281abf3ae772cc359ca80afc748
omnia <anaomnia47@gmail.com>
2025-07-19 21:42:33 +0300

    add cloudinary

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,4 +1,6 @@
 import {getUserProfile, updateUserProfile} from '../services/profileService.js';
+import cloudinary from '../utils/cloudinary.js';
+import fs from 'fs';
 //get user profile by ID
 export const getUser=async (req, res) =>{
       try {
@@ -10,15 +12,23 @@ export const getUser=async (req, res) =>{
 }
 // Update user profile
 export const updateUser = async (req, res) => {
- try {
-   const host = process.env.BASE_URL || `${req.protocol}://${req.get('host')}`;
-const imagePath = req.file ? `${host}/uploads/${req.file.filename}` : null;
+  try {
+    let imagePath = null;
 
+    if (req.file) {
+      
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: 'user_profiles', 
+      });
+
+      imagePath = result.secure_url;
+      fs.unlinkSync(req.file.path);
+    }
 
     const updatedUser = await updateUserProfile(req.user.id, req.body, imagePath);
     res.status(200).json({ message: 'Profile updated', user: updatedUser });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
-}
+};
   
